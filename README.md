fd77cc623c57a18cfe42d899fc73d02022d96b9b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-04-27 10:56:23 +0700

    Sendmail worker implemented!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sendmail-worker
 BIST League backend sendmail worker
+
+### Environment Variables
+```dotenv
+SENDGRID_API_KEY=<sendgrid_api_key>
+TEMPLATE_QUERY_STRING=<e.g. template1=template_id&template2=template_id>
+DEFAULT_FROM_ADDRESS=<default from address, e.g. noreply@bistleague.com>
+DEFAULT_FROM_NAME=<default from name, e.g. BIST League 2.0>
+```
+
+### Deployment to Cloud Functions
+```
+gcloud functions deploy sendmail-beta --runtime nodejs10 --entry-point sendMailReceived --env-vars-file .env.yaml --trigger-topic sendmail
+```
---
+++ main.js
@@ -0,0 +1,88 @@
+const querystring = require('querystring');
+const sgMail = require('@sendgrid/mail');
+const templates = querystring.parse(process.env.TEMPLATE_QUERY_STRING);
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+/**
+ * Sendmail Worker Function
+ *
+ * @param {object} data The event payload.
+ * @param {object} context The event metadata.
+ */
+exports.sendMailReceived = (data, context) => {
+    const pubSubMessage = data;
+    const payload = Buffer.from(pubSubMessage.data, 'base64').toString();
+    const json = JSON.parse(payload);
+
+    /**
+     * Expected request object
+     * {
+     *   from_address: '',
+     *   from_name: '',
+     *   to_address: 'email@example.com',
+     *   to_name: 'John Doe',
+     *   template: 'recovery|welcome|verify',
+     *   subject: '',
+     *   text: '',
+     *   html: '',
+     *   replyTo: '',
+     *   attachments: [
+     *     {
+     *       content: 'Some base 64 encoded attachment content',
+     *       filename: 'some-attachment.txt',
+     *       type: 'plain/text',
+     *       disposition: 'attachment',
+     *       content_id: 'mytext'
+     *     },
+     *     ...
+     *   ],
+     *   template_payload: {
+     *     ...
+     *   },
+     *   headers: {
+     *     ...
+     *   }
+     * }
+     */
+
+    // Check if dest address is empty
+    if(!json.to_address) {
+        console.log("Unable to complete request: destination email unspecified");
+        return;
+    }
+
+    // Assign default from address if not specified
+    if(!json.from_address) {
+        json.from_address = process.env.DEFAULT_FROM_ADDRESS;
+        json.from_name = process.env.DEFAULT_FROM_NAME;
+    }
+
+    // Create sendgrid payload
+    let send = {
+        to: (json.to_name) ? `${json.to_name} <${json.to_address}>` : json.to_address,
+        from: (json.from_name) ? `${json.from_name} <${json.from_address}>` : json.from_address,
+        subject: json.subject,
+        text: json.text,
+        html: json.html,
+        attachments: json.attachments,
+        headers: json.headers,
+        replyTo: json.replyTo,
+        cc: json.cc,
+        bcc: json.bcc
+    };
+
+    // Add template parameters, if exists
+    if(json.template) {
+        if(templates[json.template]) {
+            // Template known, append
+            send.templateId = templates[json.template];
+            send.dynamic_template_data = json.template_payload
+        }
+    }
+
+    // Send mail
+    sgMail.send(send);
+
+    console.log("Sendmail request received");
+};
